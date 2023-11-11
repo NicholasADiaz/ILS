7e6514855b97661af9febfef8fcd49d1cf96f7ea
NicholasADiaz <88463655+NicholasADiaz@users.noreply.github.com>
2023-11-10 20:37:57 -0500

    We added a home page, and some FontAwesome objects

--- ILS/src/App.jsx
+++ ILS/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBook} from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGears } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css'
 
 function App() {
@@ -8,26 +14,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<div class="grid-container">
+  <div class="grid-item"> <FontAwesomeIcon icon={faUser} /> <br /> Patrons</div>
+  <div class="grid-item"> <FontAwesomeIcon icon={faBook} /> <br /> Acquisitions </div>
+  <div class="grid-item">3 </div>
+  <div class="grid-item">4</div>
+  <div class="grid-item">5</div>
+  <div class="grid-item">6</div>
+  <div class="grid-item">7</div>
+  <div class="grid-item">8</div>
+  <div class="grid-item"> <FontAwesomeIcon icon={faGears} /> <br /> Settings</div>
+</div>
+
     </>
   )
 }
