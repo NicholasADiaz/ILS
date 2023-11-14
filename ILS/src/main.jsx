1b55d1ee8b713b3345da9d5143964b6192b7f3aa
NicholasADiaz <88463655+NicholasADiaz@users.noreply.github.com>
2023-11-14 14:41:14 -0500

    rerouted base url to login url

--- ILS/src/main.jsx
+++ ILS/src/main.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import SignIn from './SignIn.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import SignIn from './SignIn.jsx';
+import Circulation from './Circulation/Circulation.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path ="/login" element={<SignIn />}></Route>
-        <Route path ="/homepage" element={<App />}></Route>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/homepage" element={<App />} />
+        <Route path="/circulation" element={<Circulation />} />
+        <Route path="/" element={<Navigate to="/login" />} />         {/* Reroutes "127.0.0.1:5173" to the /login page */}
       </Routes>
     </Router>
   </React.StrictMode>,
-)
+);
